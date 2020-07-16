1fa7bb8a048fdef44165a6f47f3bd607fbf0578e
Hari Vignesh R <6168287-hari_vignesh_r@users.noreply.gitlab.com>
2020-07-16 14:42:45 +0530

    adding files

--- bot.js
+++ bot.js
@@ -3,13 +3,13 @@ var config=require('./config');
 // console.log(config);
 var T = new Twit(config);
 params={ 
-    q: 'rainbow',
-    count: 2
+    q: 'bigil',
+    count: 100
 }
 T.get('search/tweets',params,gotData); 
  function gotData(err, data, response) {
     
-     console.log(data['statuses']);
+     console.log(data);
   }
 
 
--- bot2.js
+++ bot2.js
@@ -1,20 +1,41 @@
 var Twit=require('twit');
 var config=require('./config');
-// console.log(config);
+console.log('The follow bot started');
 var T = new Twit(config);
 
-var tweet={ 
-    status: '#piatta' 
+var stream = T.stream('statuses/filter', { track: '@harivignesh26998' });
+
+stream.on('follow',followed);
+
+function followed(events){
+    
+
+        console.log('some one followed me');
+        var name=events.source.name;
+        var screenName=events.source.screen_name;
+        console.log(name);
+        console.log(screenName);
+        tweetit('@' + screenName + 'Thank you following me! Do yo like rainbow');
+       
+
 }
 
-T.post('statuses/update',tweet,tweeted);
+function tweetit(txt){
+    
+    var tweet={ 
+        status: txt,
+    }
 
- function tweeted(err, data, response) {
+    T.post('statuses/update',tweet,tweeted);
+}
+
+
+ function tweeted(err, _data, _response) {
     if(err){
         console.log('something went wrong!!!' +err)
     }
     else{
-        console.log(data)
+        console.log('worked!!');
 
     }
   }
