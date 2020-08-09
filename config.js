83090b04d89e50f0a65dea34796aab3d164af56e
Hari Vignesh <36480078+harivignesh261998@users.noreply.github.com>
2020-08-09 08:26:21 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    consumer_key:         'GcM1H9itmj6T4krrpEhjcI9gC',
-    consumer_secret:      'QAasr2XoVskqdgTUI8sluqhpgwJxar23OYr25kw63IUxqVYpRa',
-    access_token:         '797373882097340417-O0EXNkZ0fgWW0gLWRYEJ1xJ81DyyWO1',
-    access_token_secret:  '9BSxeEdtuXMI7rF4J2TWrbMO5gm9FDJOnrUayiaWcIXhz',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  }
